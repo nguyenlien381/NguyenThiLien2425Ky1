b9e6de90115b9c64e09ad3be18db36925a123cee
nguyenlien381 <nguyenlienn22032002@gmail.com>
2024-10-17 20:36:34 +0700

    first commit

---
+++ NguyenThiLien381/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NguyenThiLien381.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
